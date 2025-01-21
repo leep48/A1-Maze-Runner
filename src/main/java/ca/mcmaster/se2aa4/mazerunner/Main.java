d163798ba6f6d332d62028d34d85a29a18661db3
leep48 <leep48@mcmaster.ca>
2025-01-21 00:31:04 -0500

    feat: added -i flag to parse arguments using Apache CLI

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -10,24 +10,41 @@ import org.apache.commons.cli.*;
 public class Main {
 
     private static final Logger logger = LogManager.getLogger();
+    private static Options options = new Options();
 
     public static void main(String[] args) {
         logger.info("** Starting Maze Runner");
+
+        options.addOption("i", true, "specifies the filename to be used");
+
+        CommandLineParser parser = new DefaultParser();
         try {
-            logger.info("**** Reading the maze from file " + args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                for (int idx = 0; idx < line.length(); idx++) {
-                    if (line.charAt(idx) == '#') {
-                        logger.trace("WALL ");
-                    } else if (line.charAt(idx) == ' ') {
-                        logger.trace("PASS ");
+            CommandLine cmd = parser.parse(options, args);
+
+            // Checks for -i flag
+            if(cmd.hasOption("i")) {
+                String file = cmd.getOptionValue("i");
+                //System.out.println("Testing -i flag");
+
+                logger.info("**** Reading the maze from file " + file);
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    for (int idx = 0; idx < line.length(); idx++) {
+                        if (line.charAt(idx) == '#') {
+                            //logger.trace("WALL ");
+                            System.out.print("WALL ");
+                        } else if (line.charAt(idx) == ' ') {
+                            //logger.trace("PASS ");
+                            System.out.print("PASS ");
+
                     }
                 }
                 System.out.print(System.lineSeparator());
             }
-            reader.close();
+            }
+
+            //reader.close();
         } catch(Exception e) {
             logger.error("/!\\ An error has occured /!\\");
         }
